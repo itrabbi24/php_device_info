af3f245e784744d31f2e8f453d209fc63b28705f
MD Rabbi <52894020+itrabbi24@users.noreply.github.com>
2021-11-10 15:01:22 +0600

    Add files via upload

---
+++ Device Infos.php
@@ -0,0 +1,33 @@
+
+<?php
+
+$MAC = exec('getmac');
+$MAC = strtok($MAC, ' ');
+echo  $MAC;
+?>
+
+<br>
+
+<?php
+
+function GetVolumeLabel($drive) {
+  // Try to grab the volume name
+  if (preg_match('#Volume Serial Number is (.*)\n#i', shell_exec('dir '.$drive.':'), $m)) {
+    $volname = ' ('.$m[1].')';
+  } else {
+    $volname = '';
+  }
+return $volname;
+}
+
+$serial =  str_replace("(","",str_replace(")","",GetVolumeLabel("c")));
+
+echo $serial;
+
+?>
+
+<br>
+
+<?php 
+echo getenv('COMPUTERNAME');
+ ?>
